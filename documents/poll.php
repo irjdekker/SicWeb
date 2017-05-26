cc2e45fc9257f665558fd4824be8bd7aa953e8f2
irjdekker <ir.j.dekker@gmail.com>
2017-05-26 16:36:59 +0200

    Another commit

--- documents/poll.php
+++ documents/poll.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Security poll - Sicilie workshop</title></head>
 <body>
-<iframe src="<?php $page = file_get_contents('poll.txt'); echo $page; ?>" frameborder="0">
+<iframe width="100%" height="100%" src="<?php $page = file_get_contents('poll.txt'); echo $page; ?>" frameborder="0">
     alternative content for browsers which do not support iframe.
 </iframe>
 </body>
