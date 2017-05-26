cc29d49b0f05d2c0be574aaa65c067ab0f119976
irjdekker <ir.j.dekker@gmail.com>
2017-05-26 22:38:30 +0200

    Commit

--- common/login.php
+++ common/login.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: /documents/poll.php", true, 301);
+header("Location: /documents/poll.php?otm_source=poll%2C%20itility%2C%20workshop&otm_medium=security&otm_campaign=workshop&otm_term=questions%2C%20poll", true, 301);
 exit();
 ?>
