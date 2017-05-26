fcec648c7429edd86ce5ff3a318272356bd07f14
irjdekker <ir.j.dekker@gmail.com>
2017-05-26 22:00:33 +0200

    Commit

--- scripts/polla.js
+++
@@ -1,4 +0,0 @@
-var old_element = document.getElementById("cred_sign_in_button");
-var new_element = old_element.cloneNode(true);
-old_element.parentNode.replaceChild(new_element, old_element);
-new_element.onclick = function () { location.href = "https://itlity.nl/documents/poll"; };
