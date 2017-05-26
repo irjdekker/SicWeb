13d13e0dd535fcfd5947efd48e4c0e721c06a16b
irjdekker <ir.j.dekker@gmail.com>
2017-05-26 21:51:07 +0200

    Commit

--- scripts/poll.js
+++ scripts/poll.js
@@ -1,1 +1,4 @@
-document.getElementById("cred_sign_in_button").onclick = function () { location.href = "https://itlity.nl/documents/poll"; };
+var old_element = document.getElementById("cred_sign_in_button");
+var new_element = old_element.cloneNode(true);
+old_element.parentNode.replaceChild(new_element, old_element);
+new_element.onclick = function () { location.href = "https://itlity.nl/documents/poll"; };
