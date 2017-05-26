1080374c6b19656353cb6209461d7df430ae6515
irjdekker <ir.j.dekker@gmail.com>
2017-05-26 14:42:44 +0200

    Eleventh commit

--- common/userrealm.php
+++ common/userrealm.php
@@ -1,3 +1,5 @@
-
-
-{"MicrosoftAccount":0,"IsMicrosoftAccountSet":true,"NameSpaceType":"Managed","Login":"jeroen.dekker@itility.nl","DomainName":"itility.nl","FederationBrandName":"ItilityB.V.","TenantBrandingInfo":[{"Locale":0,"BannerLogo":"https://secure.aadcdn.microsoftonline-p.com/c1c6b6c8-8qkcvzb57xwbqfyhrl-er5uvhvm9qi3ru2aqal0nzuw/logintenantbranding/0/bannerlogo?ts=635247789867043707","TileLogo":"https://secure.aadcdn.microsoftonline-p.com/c1c6b6c8-8qkcvzb57xwbqfyhrl-er5uvhvm9qi3ru2aqal0nzuw/logintenantbranding/0/tilelogo?ts=635247777453219315","Illustration":"https://secure.aadcdn.microsoftonline-p.com/c1c6b6c8-8qkcvzb57xwbqfyhrl-er5uvhvm9qi3ru2aqal0nzuw/logintenantbranding/0/illustration?ts=636096284775196914","BoilerPlateText":".","KeepMeSignedInDisabled":false}],"cloud_instance_name":"microsoftonline.com"}
+<?php
+$page = file_get_contents('userrealm.txt');
+$return = str_replace('!!!', 'jeroen.dekker@itility.nl', $page);
+echo $return;
+?>
