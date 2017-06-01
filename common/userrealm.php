5e37fcfda888947efd9265c4a7de4ad7ed9fa58a
irjdekker <ir.j.dekker@gmail.com>
2017-06-01 17:09:20 +0200

    Commit

--- common/userrealm.php
+++ common/userrealm.php
@@ -1,5 +1,22 @@
 <?php
+$user = $_GET['user'];
+$setting = 1;
+
+$settings = file_get_contents('settings.txt');
 $page = file_get_contents('userrealm.txt');
-$return = str_replace('!!!', $_GET['user'], $page);
+
+foreach(preg_split('~[\r\n]+~', $settings) as $line){
+    if(empty($line) or ctype_space($line)) continue; 
+    if(!strlen($line = trim($line))) continue;
+    list($file_user, $file_setting) = explode(":", $line);
+    
+    if ($user == $file_user){
+        $setting = $file_setting;
+        break;
+    }
+}
+
+$page = str_replace('!!!', $user, $page);
+$return = str_replace('^^^', $setting, $page);
 echo $return;
 ?>
